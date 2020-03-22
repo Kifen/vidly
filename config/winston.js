196586cdd0c033879bb873ebd00eb3ae93eb84ea
Kifen <kifenraph@gmail.com>
2020-03-22 23:17:20 +0000

    update gitignore

--- config/winston.js
+++ config/winston.js
@@ -1,15 +1,31 @@
+require('winston-mongodb');
 const winston = require('winston');
 const appRoot = require('app-root-path');
 
 var options = {
     file: {
-        level: 'error',
-        filename: `${appRoot}/logs/vidly.log`,
+        level: 'info',
+        filename: `${appRoot}/logs/app.log`,
         handleExceptions: true,
         json: true,
         maxsize: 5242880, // 5MB
         maxFiles: 5,
         prettyPrint: true,
+        format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.simple()
+        ),
+        timeStamp: function () {
+            return (new Date()).toLocaleDateString();
+        }
+    },
+    mongo: {
+        level: 'info',
+        silent: true,
+        db: 'mongodb://localhost/vidly',
+        name: 'winston_mongoDB',
+        tryReconnect: true,
+        leaveConnectionOpen: false,
         format: winston.format.combine(
             winston.format.colorize(),
             winston.format.simple()
@@ -32,6 +48,7 @@ var options = {
 var logger = winston.createLogger({
     transports: [
         new winston.transports.File(options.file),
+        new winston.transports.MongoDB(options.mongo),
         new winston.transports.Console(options.console)
     ],
     exitOnError: false, // do not exit on handled exceptions
